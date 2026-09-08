72cc7d3cf348e37891512aca202287d264127e72
Thomas Korner <3245718+tkorner@users.noreply.github.com>
2026-09-08 07:16:45 +0200

    Add missing Moodle boilerplate headers to AMD source and build script
    amd/src/tag_chips.js and scripts/build-amd.js were missing the GPL
    boilerplate header required for plugin directory submissions. Moves
    the existing @module docblock in tag_chips.js above define(), matching
    core's convention (boilerplate, then file docblock, then define call),
    and fixes build-amd.js's module-id regex, which anchored on the start
    of the file and broke once a header pushed define() off line 1.
    
    Closes #3.

--- scripts/build-amd.js
+++ scripts/build-amd.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env node
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Minimal AMD build script for qbank_tagassistant, mirroring the two things Moodle core's
  * real Grunt "amd" task does to amd/src files: (1) inject the module id as the first define()
  * argument, (2) minify with a sourcemap. Run via `npm run build:amd`.
+ *
+ * @copyright  2026 TKorner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 'use strict';
 
@@ -18,7 +36,7 @@ const mappath = `${buildpath}.map`;
 
 async function build() {
     const src = fs.readFileSync(srcpath, 'utf8');
-    const named = src.replace(/^define\(\[/, `define("${component}/${modulename}", [`);
+    const named = src.replace(/define\(\[/, `define("${component}/${modulename}", [`);
 
     const result = await terser.minify(named, {
         compress: true,
